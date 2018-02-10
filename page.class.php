c98089a8b4f8306d1ffae2260814dfc95a4e575e
Luis <jr.juniorafonso@gmail.com>
2018-02-10 11:08:32 -0200

    d

---
+++ page.class.php
@@ -0,0 +1,59 @@
+<?php
+
+class Page
+{
+	var $title;
+	var $content;
+	var $css = array();
+	
+	function show ()
+	{
+		echo '<html>';
+		
+			echo '<head>';
+				$this->head();
+			echo '</head>';
+			
+			echo '<body>';
+				$this->body();
+			echo '</body>';
+			
+		echo '</html>';
+			
+	}
+	function head ()
+	{
+		echo '<title>' . $this->title . '</title>';
+		for($i = 0; $i < count($this->css); $i++)
+		{
+		echo $this->css[$i];
+		}	
+	}
+	
+	function body ()
+	{
+		echo '<h1>' . $this->title . '</h1>';
+		echo '<p>' . $this->content . '</p>';
+	}
+	function css ($css)
+	{
+	
+		if(is_array($css)){
+			for($i=0; $i < count($css); $i++)
+			{
+			$this->css[] = '<link href="' . $css [$i]. '" rel="stylesheet">';
+			}
+		}else{
+			$this->css[] = '<link href="' . $css . '"rel="stylesheet">';
+		}
+	}
+}
+
+			
+				
+	$home= new page();
+	$home->title='INICIO';
+	$home->content ='PRIMEIRA LINHA';
+	$home->css('style.css');
+	$home->show();
+	
